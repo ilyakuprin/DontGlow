2cb07f0e0bf770273cb981b85300d019cc9ae03f
ilyakuprin <ilia6478@gmail.com>
2024-04-26 11:26:26 +0300

    showing key
    Ключ в UI появляется, когда его подбираешь

--- Assets/_DontGlow/Scripts/UI/Game/ShowingKey.cs
+++ Assets/_DontGlow/Scripts/UI/Game/ShowingKey.cs
@@ -1,25 +1,26 @@
 using System;
+using _DontGlow.Scripts.MainHero;
 using Zenject;
 
 namespace _DontGlow.Scripts.UI.Game
 {
     public class ShowingKey : IInitializable, IDisposable
     {
-        private readonly ShowingCountNote _showingCountNote;
+        private readonly PickingUpItems _pickingUpItems;
         private readonly UiInGameView _uiInGameView;
 
-        public ShowingKey(ShowingCountNote showingCountNote,
+        public ShowingKey(PickingUpItems pickingUpItems,
                           UiInGameView uiInGameView)
         {
-            _showingCountNote = showingCountNote;
+            _pickingUpItems = pickingUpItems;
             _uiInGameView = uiInGameView;
         }
 
         public void Initialize()
-            => _showingCountNote.LastNodeTaken += Show;
+            => _pickingUpItems.TakenKey += Show;
 
         public void Dispose()
-            => _showingCountNote.LastNodeTaken -= Show;
+            => _pickingUpItems.TakenKey -= Show;
 
         private void Show()
             => _uiInGameView.Key.SetActive(true);
